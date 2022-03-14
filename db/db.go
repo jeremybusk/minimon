f434a4ce93b3936db33a5e4a49a07566df5f120d
Jeremy Busk <jeremybusk@gmail.com>
2022-03-14 13:05:36 -0600

    try

--- db/db.go
+++ db/db.go
@@ -6,21 +6,22 @@ import (
     //"github.com/uvoo/minimon/pkg/models"
     "gorm.io/driver/postgres"
     "gorm.io/gorm"
-	"fmt"
+	// "fmt"
 "os"
 )
 
 var DB *gorm.DB
 
-func Open() *gorm.DB {
+// func Open() *gorm.DB {
+func Open() {
    var err error
    // DB, err = gorm.Open("postgres", "host=localhost port=5432 user=someUser dbname=someDB password=somePW sslmode=disable")
    dbURL := os.Getenv("MINIMON_DBURL")
-    DB, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+    DB, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
    if err != nil {
        log.Fatalln(err)
        // return err
    }
    // fmt.Printf("%v", DB)
-   return DB
+   // return DB
 }
