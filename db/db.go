740039012875ea2582955bcfa76aa50b35e0edfb
Jeremy Busk <jeremybusk@gmail.com>
2022-03-14 12:20:34 -0600

    update

--- db/db.go
+++ db/db.go
@@ -6,11 +6,13 @@ import (
     //"github.com/uvoo/minimon/pkg/models"
     "gorm.io/driver/postgres"
     "gorm.io/gorm"
+	"fmt"
 )
 
 var DB *gorm.DB
 
-func Init(dbURL string) *gorm.DB {
+// func Init(dbURL string) *gorm.DB {
+func Init(dbURL string) {
     // dbURL := "postgres://pg:pass@localhost:5432/crud"
 
     DB, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
@@ -18,12 +20,9 @@ func Init(dbURL string) *gorm.DB {
     if err != nil {
         log.Fatalln(err)
     }
+	fmt.Printf("%v", DB)
 
     // db.AutoMigrate(&models.Book{})
 
-    return db
+    // return db
 }
-
-func Close() error {
-  return DB.Close()
-  }
