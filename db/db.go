2807497c6af509b8074345d60d84db61f6f71189
Jeremy Busk <jeremybusk@gmail.com>
2022-03-14 12:45:16 -0600

    update db pkg

--- db/db.go
+++ db/db.go
@@ -7,22 +7,19 @@ import (
     "gorm.io/driver/postgres"
     "gorm.io/gorm"
 	"fmt"
+"os"
 )
 
 var DB *gorm.DB
 
-// func Init(dbURL string) *gorm.DB {
-func Init(dbURL string) {
-    // dbURL := "postgres://pg:pass@localhost:5432/crud"
-
+func Open() {
+   var err error
+   // DB, err = gorm.Open("postgres", "host=localhost port=5432 user=someUser dbname=someDB password=somePW sslmode=disable")
+   dbURL := os.Getenv("MINIMON_DBURL")
     DB, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
-
-    if err != nil {
-        log.Fatalln(err)
-    }
-	fmt.Printf("%v", DB)
-
-    // db.AutoMigrate(&models.Book{})
-
-    // return db
+   if err != nil {
+       log.Fatalln(err)
+       // return err
+   }
+   fmt.Printf("%v", DB)
 }
